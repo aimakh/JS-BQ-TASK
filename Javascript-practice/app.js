e319a92958fcec0c727f89ebfec4bc5d77441952
Aima Khan <aimak748@gmail.com>
2023-04-06 02:08:13 +0100

    Task Updated

---
+++ Javascript-practice/app.js
@@ -0,0 +1,26 @@
+// Day 1 - 04 April 2023
+
+let firstName = "Aima";
+let lastName  = "Khan";
+let data1 = firstName + lastName;
+console.log(data1);
+
+let userName = prompt("What is your Name?");
+document.write("Welcome to my website " + userName);
+
+let userFavNum = prompt("What is your favourite color?");
+console.log(userFavNum);
+
+
+let data = 10;
+let data2 = 7;
+console.log(data * data2);
+
+alert("Hello World");
+
+
+let userNum1 = prompt("Enter first number to be added");
+let userNum2 = prompt("Enter second number to be added");
+
+let result = userNum1 + userNum2;
+console.log(result);
